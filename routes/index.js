380fd5634222fd7c761269463e8797c41379b7aa
seriousfog <122305069+seriousfog@users.noreply.github.com>
2026-03-18 22:27:07 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -283,12 +283,22 @@ router.post('/registeruser', addUserToViews, async function (req, res) {
 const passport = require('passport');
 
 
-router.post('/login', addUserToViews, passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      failureMessage: true
-    })
-);
+router.post('/login', function(req, res, next) {
+
+  passport.authenticate('local', function(err, user, info) {
+    if (err) return next(err);
+    if (!user) {
+      return res.render('login', {
+        title: 'Login User',
+        error: 'Incorrect email or password'
+      });
+    }
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
 
 router.get('/login', addUserToViews, function (req, res) {
   res.render('login', { title: 'Login User' });
